45fee28e915126c803f273d8589a5cdeb7a53fc9
djk <djk@nqiosdeimac.local>
2017-02-23 14:48:00 +0800

    00.友盟平台Demo创建

---
+++ YMDemo/YMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YMDemo
+//
+//  Created by djk on 17/2/23.
+//  Copyright © 2017年 NQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YMDemo/YMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YMDemo
+//
+//  Created by djk on 17/2/23.
+//  Copyright © 2017年 NQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
